cc2d9ad6a27e1abf9cdeff8e8a3d4e51624e4c7a
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-04-11 00:45:39 +0000

    Some additions to the parameter parser

--- src/parameters.hs
+++ src/parameters.hs
@@ -1,4 +1,29 @@
 module BzoParameterParser where
+import Data.List
+
+
+
+
+
+
+
+
+
+
+data OptimizationSettings = Opt_OL1 | Opt_OL2 | Opt_OL3 | Opt_OL4 |          -- | Latency
+                            Opt_OS1 | Opt_OS2 | Opt_OS3 | Opt_OS4 |          -- | Size
+                            Opt_OT1 | Opt_OT2 | Opt_OT3 | Opt_OT4 | Opt_Nil  -- | Throughput, Nil
+
+
+
+
+
+
+
+
+
+
+data PrefixFlags = PathFlag FilePath | GranFlag Int | OutFlag FilePath
 
 
 
@@ -12,10 +37,11 @@ module BzoParameterParser where
 data BzoSettings
   = BzoSettings {
     importedFiles :: [(FilePath, String)],
-    linkedFiles   :: [(FilePath, String)],
+    libraryFiles  :: [(FilePath, String)],
     flags         :: [String],
-    parFlags      :: [(String, FilePath)],
-    prefixFlags   :: [(String, String)] }
+    optFlags      :: OptimizationSetting,
+    parFlags      :: [(String, FilePath],
+    prefixFlags   :: [PrefixFlags] }
 
 
 
@@ -41,11 +67,11 @@ parseFlag s = case (head s) of
 
 
 parsePrefixFlag :: String -> Maybe (String, String)
-parsePrefixFlag s = case (parseFlag s) of
-  Nothing -> Nothing
-  Just s' -> case (s' !! 2) of
-    'l' -> Just $ drop 2 s'
-    _   -> Nothing
+parsePrefixFlag s
+  | isPrefixOf "-p=" s = Just $ drop 3 s    -- Path to search for missing files
+  | isPrefixOf "-g=" s = Just $ drop 3 s    -- Thread Granularity
+  | isPrefixOf "-o=" s = Just $ drop 3 s    -- Output Path
+  | otherwise          = Nothing
 
 
 
@@ -56,5 +82,37 @@ parsePrefixFlag s = case (parseFlag s) of
 
 
 
-parseParameters :: [String] -> BzoSettings
-parseParameters =
+parseOptimization :: String -> Maybe OptimizationSettings
+parseOptimization s =
+  case s of
+    "-O"   -> Just Opt_OT2
+    "-O0"  -> Just Opt_Nil
+    "-O1"  -> Just Opt_OT1
+    "-O2"  -> Just Opt_OT2
+    "-O3"  -> Just Opt_OT3
+    "-O4"  -> Just Opt_OT4
+    "-OT1" -> Just Opt_OT1
+    "-OT2" -> Just Opt_OT2
+    "-OT3" -> Just Opt_OT3
+    "-OT4" -> Just Opt_OT4
+    "-OS1" -> Just Opt_OS1
+    "-OS2" -> Just Opt_OS2
+    "-OS3" -> Just Opt_OS3
+    "-OS4" -> Just Opt_OS4
+    "-OL1" -> Just Opt_OL1
+    "-OL2" -> Just Opt_OL2
+    "-OL3" -> Just Opt_OL3
+    "-OL4" -> Just Opt_OL4
+    _      -> Nothing
+
+
+
+
+
+
+
+
+
+
+--parseParameters :: [String] -> BzoSettings
+--parseParameters =
