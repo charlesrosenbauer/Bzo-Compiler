962bdc74a42f05a4771910eaf835825d7c5ce8ba
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-06-06 23:19:18 +0000

    Needed a fresh start with this typechecker code

--- src/typechecker.hs
+++ src/typechecker.hs
@@ -6,7 +6,7 @@ import GHC.Exts
 import Data.Either
 import Data.Maybe
 import Data.List hiding (map, foldl, foldr, insert)
-import Data.Map.Strict hiding (map, foldl, foldr)
+import Data.Map.Strict hiding (map, foldl, foldr, mapEither)
 import HigherOrder
 import Debug.Trace
 
@@ -193,6 +193,8 @@ isValidBTId _            = False
 
 
 
+
+
 getImportDependencies :: BzoFileData -> [String]
 getImportDependencies fs = nub $ (\(BzoFileData mn fp dm ast im ln ia la) -> im ++ (map fst ia)) fs
 
@@ -293,128 +295,10 @@ appendStdDep (BzoFileData mn fp dmn   ast imp lnk ima lna) =
 
 
 
--- | Returns (Ids/BI Ids, TyIds/BI TyIds, MIds)
-getIds :: BzoSyntax -> ([String], [String], [String])
-getIds (BzS_Expr       p xs) = zip3Map $ map getIds xs
-getIds (BzS_Block      p xs) = zip3Map $ map getIds xs
-getIds (BzS_MId        p st) = ([  ], [  ], [st])
-getIds (BzS_TyId       p st) = ([  ], [st], [  ])
-getIds (BzS_Id         p st) = ([st], [  ], [  ])
-getIds (BzS_BId        p st) = ([st], [  ], [  ])
-getIds (BzS_BTId       p st) = ([  ], [st], [  ])
-getIds (BzS_Filter     p xs) = getIds xs
-getIds (BzS_Box        p xs) = getIds xs
-getIds (BzS_ArrExprMod p xs) = getIds xs
-getIds (BzS_Cmpd       p xs) = zip3Map $ map getIds xs
-getIds (BzS_Poly       p xs) = zip3Map $ map getIds xs
-getIds (BzS_FnTy    p as bs) = zip3Map [getIds as, getIds bs]
-getIds (BzS_Calls      p xs) = zip3Map $ map getIds xs
-
-getIds (BzS_FunDef     p i f o d) = zip3Map [getIds i, getIds o, getIds d, ([f], [ ], [ ])]
-getIds (BzS_FnTypeDef  p   f   d) = zip3Map [                    getIds d, ([f], [ ], [ ])]
-getIds (BzS_TypDef     p i t   d) = zip3Map [getIds i,           getIds d, ([ ], [t], [ ])]
-
-getIds _                     = ([  ], [  ], [  ])
-
-
-
-
-
-
-
-
-
-
-extractOutVarIds :: BzoSyntax -> Maybe [String]
-extractOutVarIds (BzS_Id     p           st ) = Just [st]
-extractOutVarIds (BzS_Box    p (BzS_Id o st)) = Just [st]
-extractOutVarIds (BzS_Cmpd   p           xs ) =
-  case (concat $ catMaybes $ map extractOutVarIds xs) of
-    [] -> Nothing
-    ms -> Just ms
-extractOutVarIds (BzS_Expr   p           xs ) = fn Nothing (reverse xs)
-  where fn :: Maybe [String] -> [BzoSyntax] -> Maybe [String]
-        fn (Just xs)                       _  = Just xs
-        fn Nothing  ((BzS_Filter    p fl):xs) = fn Nothing xs
-        fn Nothing  ((BzS_Namespace p nm):xs) = fn Nothing xs
-        fn Nothing  ((BzS_Id        p x ):xs) = Just [x]
-        fn Nothing  ((BzS_BId       p x ):xs) = Just [ ]
-        fn Nothing  ((BzS_BTId      p x ):xs) = Just [ ]
-        fn Nothing  ((BzS_Wildcard  p   ):xs) = Just [ ]
-        fn Nothing                         _  = Nothing
-
-
-
-
-
-
-
-
-
-
 -- | Return (Function Names, Type Names, Fn Types)
-getCallIds :: BzoSyntax -> ([(String, [BzoSyntax])], [(String, [BzoSyntax])], [(String, [BzoSyntax])])
+getCallIds :: BzoSyntax -> ([(String, BzoSyntax)], [(String, BzoSyntax)], [(String, BzoSyntax)])
 getCallIds (BzS_Calls     p      xs) = zip3Map $ map getCallIds xs
-getCallIds (BzS_FnTypeDef p   f   d) = ([                              ], [                           ], [(f, [BzS_FnTypeDef p   f   d])])
-getCallIds (BzS_TypDef    p i t   d) = ([                              ], [(t, [BzS_TypDef p i t   d])], [                              ])
-getCallIds (BzS_FunDef    p i f o d) = ([(f, [BzS_FunDef    p i f o d])], [                           ], [                              ])
-getCallIds _                         = ([                              ], [                           ], [                              ])
-
-
-
-
-
-
-
-
-
-
-getTypeData0 :: BzoSyntax -> BzoFileTypeData
-getTypeData0 ast =
-  let (fns , tys , fts ) = getCallIds ast
-      (fns', tys', fts') = (shrinkPairs fns, shrinkPairs tys, shrinkPairs fts)
-  in (BzoFileTypeData "" "" ast fns' tys' fts' [] [] [])
-
-
-
-
-
-
-
-
-
-
--- Adjust to return errors
-modelType :: BzoSyntax -> BzoType
-modelType (BzS_Cmpd ps exprs) = TyCmpd $ map modelType exprs
-modelType (BzS_Poly ps exprs) = TyPoly $ map modelType exprs
-modelType (BzS_Wildcard ps  ) = TyWild
-modelType (BzS_Nil      ps  ) = TyNil
-modelType (BzS_Str  ps  str ) = LtStr str
-modelType (BzS_Int  ps  num ) = LtInt num
-modelType (BzS_Flt  ps  num ) = LtFlt num
-modelType (BzS_Id   ps  str ) = LtId  str   -- Later, check if id is a global function
-modelType (BzS_BId  ps  str ) = LtId  str   -- Later, check if id is a valid builtin
-modelType (BzS_TyId ps  str ) = LtTy  str   -- Later, make Type or TyVar depending on if str is globally defined
-modelType (BzS_BTId ps  str ) = LtTy  str   -- Later, check if id is a valid builtin
---modelType (BzS_MId  ps  str ) = ERR
---modelType (BzS_Expr ps atoms) =
-
-
-
-
-
-
-
-
-
-
-{-
-generateSymbolTables :: [BzoFileData] -> Either [BzoErr] [BzoFileTypeData]
-generateSymbolTables fd =
-  let t0 = map (getTypeData0 . bfd_fileAST) fd
-      t1 = map bfd_moduleName fd
-      t2 = map bfd_domain     fd
-      t3 = map (\(mn, dm, (BzoFileTypeData _ _ ast fns tys fts _ _ _)) -> (BzoFileTypeData mn dm ast fns tys fts [] [] []))  $ zip3 t0 t1 t2
-      t4 = insertMany empty $ shrinkPairs $ map (\(BzoFileTypeData mn dm _ fns tys fts _ _ _) -> (dm, )) fd
--}
+getCallIds (BzS_FnTypeDef p   f   d) = ([                            ], [                         ], [(f, BzS_FnTypeDef p   f   d)])
+getCallIds (BzS_TypDef    p i t   d) = ([                            ], [(t, BzS_TypDef p i t   d)], [                            ])
+getCallIds (BzS_FunDef    p i f o d) = ([(f, BzS_FunDef    p i f o d)], [                         ], [                            ])
+getCallIds _                         = ([                            ], [                         ], [                            ])
