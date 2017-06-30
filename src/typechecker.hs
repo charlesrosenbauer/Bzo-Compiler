7f7506aaa55c01b01f1058c0e1a07a708cedcaed
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-06-30 23:43:22 +0000

    I think this would be better in the type checker.

--- src/typechecker.hs
+++ src/typechecker.hs
@@ -20,6 +20,120 @@ import Debug.Trace
 
 
 
+isValidBId :: String -> Bool
+isValidBId "$add-binop"    = True
+isValidBId "$sub-binop"    = True
+isValidBId "$mul-binop"    = True
+isValidBId "$div-binop"    = True
+isValidBId "$mod-binop"    = True
+isValidBId "$gtr-binop"    = True
+isValidBId "$lss-binop"    = True
+isValidBId "$geq-binop"    = True
+isValidBId "$leq-binop"    = True
+isValidBId "$eql-binop"    = True
+isValidBId "$neq-binop"    = True
+isValidBId "$arrLength"    = True
+isValidBId "$getIndex"     = True
+isValidBId "$setIndex"     = True
+isValidBId "$map"          = True
+isValidBId "$fold"         = True
+isValidBId "$reduce"       = True
+isValidBId "$scam"         = True
+isValidBId "$chain"        = True
+isValidBId "$zip"          = True
+isValidBId "$exp-binop"    = True
+isValidBId "$surd-binop"   = True
+isValidBId "$log-binop"    = True
+isValidBId "$sine"         = True
+isValidBId "$cosine"       = True
+isValidBId "$tangent"      = True
+isValidBId "$arcsine"      = True
+isValidBId "$arccosine"    = True
+isValidBId "$arctangent"   = True
+isValidBId "$hsine"        = True
+isValidBId "$hcosine"      = True
+isValidBId "$htangent"     = True
+isValidBId "$arc-hsine"    = True
+isValidBId "$arc-hcosine"  = True
+isValidBId "$arc-htangent" = True
+isValidBId "$floor"        = True
+isValidBId "$ciel"         = True
+isValidBId "$round"        = True
+isValidBId "$factorial"    = True
+isValidBId "$gamma"        = True
+isValidBId "$nCr"          = True
+isValidBId "$nPr"          = True
+isValidBId "$sqrt"         = True
+isValidBId "$cbrt"         = True
+isValidBId "$log2"         = True
+isValidBId "$log10"        = True
+isValidBId "$to-uppercase" = True
+isValidBId "$to-lowercase" = True
+isValidBId "$isPrefix"     = True
+isValidBId "$isSuffix"     = True
+isValidBId "$append-array" = True
+isValidBId "$print"        = True
+isValidBId "and-binop"     = True
+isValidBId "$or-binop"     = True
+isValidBId "$xor-binop"    = True
+isValidBId "$not-op"       = True
+isValidBId "$rshift-binop" = True
+isValidBId "$lshift-binop" = True
+isValidBId "$ctlz-op"      = True
+isValidBId "$cttz-op"      = True
+isValidBId "$popcount-op"  = True
+isValidBId "$toInt"        = True
+isValidBId "$toUnt"        = True
+isValidBId "$toUnm"        = True
+isValidBId "$toFlt"        = True
+isValidBId "$toBits"       = True
+isValidBId _               = False
+
+
+
+
+
+
+
+
+
+
+isValidBTId :: String -> Bool
+isValidBTId "$Int8"      = True
+isValidBTId "$Int16"     = True
+isValidBTId "$Int32"     = True
+isValidBTId "$Int64"     = True
+isValidBTId "$Unt8"      = True
+isValidBTId "$Unt16"     = True
+isValidBTId "$Unt32"     = True
+isValidBTId "$Unt64"     = True
+isValidBTId "$Flt16"     = True
+isValidBTId "$Flt32"     = True
+isValidBTId "$Flt64"     = True
+isValidBTId "$Unm16"     = True
+isValidBTId "$Unm32"     = True
+isValidBTId "$Unm64"     = True
+isValidBTId "$BoolTF"    = True
+isValidBTId "$BoolFT"    = True
+isValidBTId "$Char"      = True
+isValidBTId "$ASCII"     = True
+isValidBTId "$Unique"    = True
+isValidBTId "$Effect"    = True
+isValidBTId "$Byte"      = True
+isValidBTId "$Half"      = True
+isValidBTId "$Word"      = True
+isValidBTId "$Dword"     = True
+isValidBTId _            = False
+
+
+
+
+
+
+
+
+
+
 data BzoLiteral
   = LtStr String
   | LtInt Integer
