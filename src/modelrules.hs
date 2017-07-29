a7e05f73b8adc73646e7786eca9325e39597aae1
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-07-29 18:28:36 +0000

    Rewriting the modeller from scratch again. Barely anything there anyway.

--- src/modelrules.hs
+++ src/modelrules.hs
@@ -14,128 +14,63 @@ import Data.Maybe
 
 
 
-data BzoRecord
-  = BzoRecord {
-      br_pos :: BzoPos,
-      br_nm  :: String,
-      br_ty  :: BzoSyntax }
-
-
-
-
-
-
-
-
-
-
-data BzoEnum
-  = BzoEnum {
-      be_pos :: BzoPos,
-      be_nm  :: String,
-      be_ty  :: BzoSyntax }
-
-
-
-
-
-
-
-
-
-
-data ModelTypeAtom
-  = MTA_Int {
-      mta_pos :: BzoPos,
-      mta_int :: Integer }
-  | MTA_Flt {
-      mta_pos :: BzoPos,
-      mta_flt :: Double }
-  | MTA_Str {
-      mta_pos :: BzoPos,
-      mta_str :: String }
-  | MTA_Var {
-      mta_pos :: BzoPos,
-      mta_id  :: String,
-      mta_filt:: Maybe ModelType }
-  | MTA_Fun {
-      mta_pos :: BzoPos,
-      mta_id  :: String }
-  | MTA_Type {
-      mta_pos :: BzoPos,
-      mta_id  :: String,
-      mta_filt:: Maybe ModelType }
-  | MTA_Nil {
-      mta_pos :: BzoPos }
-
-
-
-
-
-
-
-
-
-
-data ModelArrayType
-  = ModelTypeGenArr {
-      mat_pos  :: BzoPos }
-  | ModelTypeIntArr {
-      mat_pos  :: BzoPos,
-      mat_size :: Integer }
-
-
-
-
-
-
-
-
-
-
-data ModelFilterTy = Maybe ModelType
-
-
-
-
-
-
-
-
-
-
-data ModelType
-  = MT_Cmpd {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_exp :: [ModelType] }
-  | MT_Poly {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_exp :: [ModelType] }
-  | MT_Enum {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_exp :: [ModelType],
-      mt_enm :: [ModelEnum] }
-  | MT_Record {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_exp :: [ModelType],
-      mt_rcd :: [ModelRecord] }
-  | MT_TyExpr {
-      mt_pos :: BzoPos,
-      mt_ths :: ModelTypeAtom,
-      mt_nxt :: ModelType }
-  | MT_TyAtom {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_atm :: ModelTypeAtom }
-  | MT_FnTy {
-      mt_pos :: BzoPos,
-      mt_arr :: Maybe [ModelArrayType],
-      mt_in  :: ModelType,
-      mt_out :: ModelType }
+data TypeAST
+      = TA_Cmpd {
+          ta_pos :: BzoPos,
+          ta_exs :: [TypeAST] }
+      | TA_Poly {
+          ta_pos :: BzoPos,
+          ta_exs :: [TypeAST] }
+      | TA_Expr {
+          ta_pos :: BzoPos,
+          ta_exp :: TypeAST }
+      | TA_Filt {
+          ta_pos :: BzoPos,
+          ta_filt :: TypeAST,
+          ta_exp  :: TypeAST }
+      | TA_FnTy {
+          ta_pos :: BzoPos,
+          ta_in   :: TypeAST,
+          ta_out  :: TypeAST }
+      | TA_Enum {
+          ta_pos :: BzoPos,
+          ta_id   :: String,
+          ta_exp  :: TypeAST }
+      | TA_Record {
+          ta_pos :: BzoPos,
+          ta_id   :: String,
+          ta_exp  :: TypeAST }
+      | TA_Curry {
+          ta_pos :: BzoPos,
+          ta_crs  :: [TypeAST],
+          ta_exp  :: TypeAST }
+      | TA_Arr {
+          ta_pos :: BzoPos,
+          ta_szs  :: [Int],
+          ta_exp  :: TypeAST }
+      | TA_IntLit {
+          ta_pos :: BzoPos,
+          ta_int  :: Integer }
+      | TA_FltLit {
+          ta_pos :: BzoPos,
+          ta_flt  :: Double }
+      | TA_StrLit {
+          ta_pos :: BzoPos,
+          ta_str  :: String }
+      | TA_FnLit  {
+          ta_pos :: BzoPos,
+          ta_fn   :: String }
+      | TA_TyLit  {
+          ta_pos :: BzoPos,
+          ta_ty   :: String }
+      | TA_BFnLit {
+          ta_pos :: BzoPos,
+          ta_fn   :: String }
+      | TA_BTyLit {
+          ta_pos :: BzoPos,
+          ta_ty   :: String }
+      | TA_Nil{
+          ta_pos :: BzoPos }
 
 
 
@@ -147,9 +82,10 @@ data ModelType
 
 
 data ModelRecord = ModelRecord{
-      mr_pos  :: BzoPos,
-      mr_name :: String,
-      mr_type :: ModelType }
+    mr_pos    :: BzoPos,
+    mr_name   :: String,
+    mr_parent :: String,
+    mr_type   :: TypeAST }
 
 
 
@@ -161,9 +97,10 @@ data ModelRecord = ModelRecord{
 
 
 data ModelEnum = ModelEnum{
-    me_pos  :: BzoPos,
-    me_name :: String,
-    me_type :: ModelType }
+    me_pos    :: BzoPos,
+    me_name   :: String,
+    me_parent :: String,
+    me_type   :: TypeAST }
 
 
 
@@ -174,122 +111,42 @@ data ModelEnum = ModelEnum{
 
 
 
-modelRecord :: BzoSyntax -> Either [BzoErr] ModelRecord
-modelRecord (BzS_Expr p [(BzS_Id _ i), (BzS_Filter _ t)]) =
-  case (modelType t) of
-    Left errs -> Left  errs
-    Right typ -> Right (ModelRecord p i typ)
-modelRecord syn = Left [(ModelErr (pos syn) "Invalid Record")]
+-- | Basic Type Expression. No Records or Enums. Used for filters, etc.
+modelBasicType :: BzoSyntax -> Either [BzoErr] TypeAST
+modelBasicType (BzS_Int  p i)  = Right (TA_IntLit p i)
+modelBasicType (BzS_Flt  p f)  = Right (TA_FltLit p f)
+modelBasicType (BzS_Str  p s)  = Right (TA_StrLit p s)
 
+modelBasicType (BzS_FnTy p i e) =
+  let i'  = [modelBasicType i]
+      e'  = [modelBasicType e]
+      ers = (lefts  i') ++ (lefts e')
+      vli = head $ rights i'
+      vle = head $ rights e'
+  in case ers of
+      [] -> Right (TA_FnTy p vli vle)
+      er -> Left $ concat er
 
+modelBasicType (BzS_Cmpd p xs) =
+  let xs' = [map modelBasicType xs]
+      ers = concatMap lefts  xs'
+      vls = concatMap rights xs'
+  in case ers of
+      [] -> Right (TA_Cmpd p vls)
+      er -> Left $ concat er
 
+modelBasicType (BzS_Poly p xs) =
+  let xs' = [map modelBasicType xs]
+      ers = concatMap lefts  xs'
+      vls = concatMap rights xs'
+  in case ers of
+      [] -> Right (TA_Poly p vls)
+      er -> Left $ concat er
 
-
-
-
-
-
-
-modelEnum :: BzoSyntax -> Either [BzoErr] ModelEnum
-modelEnum (BzS_Expr p [(BzS_TyId _ i), (BzS_Filter _ t)]) =
-  case (modelType t) of
-    Left errs -> Left  errs
-    Right typ -> Right (ModelEnum p i typ)
-modelEnum syn = Left [(ModelErr (pos syn) "Invalid Enum")]
-
-
-
-
-
-
-
-
-
-
-modelSimpleTypeParameter :: BzoSyntax -> Either [BzoErr] ModelType
-modelSimpleTypeParameter (BzS_Expr p0 [(BzS_Cmpd  p1 xs)]) =
-  let xs' = map modelSimpleTypeParameter xs
-      es  = lefts  xs'
-      ms  = rights xs'
-  in case (es, ms) of
-      ([], ty) -> Right (MT_Cmpd p0 Nothing ty)
-      (er, ty) -> Left $ concat er
-modelSimpleTypeParameter (BzS_Expr p0 [(BzS_TyVar p1 i)]) = Right (MT_TyAtom p0 Nothing (MTA_Var p1 i Nothing))
-modelSimpleTypeParameter (BzS_Expr p0 [(BzS_TyVar p1 i), (BzS_Filter p2 flt)]) =
-  case (modelType flt) of
-    Left errs -> Left errs
-    Right typ -> Right (MT_TyAtom p0 Nothing (MTA_Var p1 i (Just typ)))
-modelSimpleTypeParameter x = Left [TypeErr (pos x) "Invalid Type Parameters"]
-
-
-
-
-
-
-
-
-
-
-modelArrayIndicator :: BzoSyntax -> ModelArrayType
-modelArrayIndicator (BzS_ArrSzObj p s) = (ModelTypeIntArr p s)
-modelArrayIndicator (BzS_ArrGnObj p  ) = (ModelTypeGenArr p  )
-
-
-
-
-
-
-
-
-
-
-{-
-  TODO:
-    * Filter, Curry, Array Object Modelling
-    * Type Expression Modeller
--}
-modelType :: BzoSyntax -> Either [BzoErr] ModelType
-modelType (BzS_Cmpd p xs) =
-  let xs' = map (eitherFirst modelRecord modelType) xs
-      ers = aChoices xs'
-      rcs = bChoices xs'
-      tps = cChoices xs'
-  in case (ers, rcs, tps) of
-        ([], [], ts) -> Right (MT_Cmpd   p Nothing ts)
-        ([], rs, ts) -> Right (MT_Record p Nothing ts rs)
-        (er, _ , _ ) -> Left  $ concat er
-
-modelType (BzS_Poly p xs) =
-  let xs' = map (eitherFirst modelEnum modelType) xs
-      ers = aChoices xs'
-      ens = bChoices xs'
-      tps = cChoices xs'
-  in case (ers, ens, tps) of
-        ([], [], ts) -> Right (MT_Poly p Nothing ts)
-        ([], es, ts) -> Right (MT_Enum p Nothing ts es)
-        (er, _ , _ ) -> Left  $ concat er
-
-modelType (BzS_FnTy p i e) =
-  let x   = modelType i
-      y   = modelType e
-      ers = lefts  ([x] ++ [y])
-      tx  = rights $ [x]
-      ty  = rights $ [y]
-  in case (ers, tx, ty) of
-      ([], [i'], [e']) -> Right (MT_FnTy p Nothing i' e')
-      (er, _   , _   ) -> Left  $ concat er
-
-modelType (BzS_Expr  p [i]) = modelType i
-modelType (BzS_Box   p  i ) = modelType i
-
--- General BzS_Expr modeller should probably make a linked-list-like model from MT_TyExpr. Will have to do that after vacation.
-
-modelType (BzS_TyVar p i) = Right (MT_TyAtom p Nothing (MTA_Var  p i))
-modelType (BzS_Int   p i) = Right (MT_TyAtom p Nothing (MTA_Int  p i))
-modelType (BzS_Str   p s) = Right (MT_TyAtom p Nothing (MTA_Str  p s))
-modelType (BzS_Flt   p f) = Right (MT_TyAtom p Nothing (MTA_Flt  p f))
-modelType (BzS_Nil   p  ) = Right (MT_TyAtom p Nothing (MTA_Nil  p  ))
-modelType (BzS_Id    p i) = Right (MT_TyAtom p Nothing (MTA_Fun  p i))
-modelType (BzS_BId   p i) = Right (MT_TyAtom p Nothing (MTA_Fun  p i))
-modelType (BzS_TyId  p i) = Right (MT_TyAtom p Nothing (MTA_Type p i Nothing))
-modelType (BzS_BTId  p i) = Right (MT_TyAtom p Nothing (MTA_Type p i Nothing))
+modelBasicType (BzS_Box p x) =
+  let xs' = [[modelBasicType x]]
+      ers = concatMap lefts  xs'
+      vls = concatMap rights xs'
+  in case ers of
+      [] -> Right $ head vls
+      er -> Left $ concat er
