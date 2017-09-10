127a2f494139154504e16f854e75b67fc0562502
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-09-10 23:43:50 +0000

    More Symbol Table functions

--- src/SymbolTable.hs
+++ src/SymbolTable.hs
@@ -45,3 +45,42 @@ addSymbol file st@(SymbolTable iids fids itab ftab itop ftop) symbol =
           Just _  -> st' -- Symbol has been used in the current file. No need for additions
           Nothing ->
             (SymbolTable (M.insert symbol ((fid, itop'+1):xs) iids') fids' (M.insert (itop'+1) (symbol, fid) itab') ftab' (itop'+1) ftop')
+
+
+
+
+
+
+
+
+
+
+addToTable' :: String -> SymbolTable -> CallAST -> SymbolTable
+addToTable' file st (CA_Calls    _ xs ) = foldl (addToTable' file) st xs
+addToTable' file st (CA_HintCall _ _ _) = st
+addToTable' file st (CA_REPLCall _ _  ) = st
+addToTable' file st ca = addSymbol (T.pack file) st (T.pack $ ca_id ca)
+
+
+
+
+
+
+
+
+
+
+addToTable :: SymbolTable -> BzoFileModel CallAST -> SymbolTable
+addToTable st (BzoFileModel mn _ dm ca _ _ _ _) = addToTable' (mn ++ ":" ++ dm) st ca
+
+
+
+
+
+
+
+
+
+
+generateSymbolTable :: [BzoFileModel CallAST] -> SymbolTable
+generateSymbolTable ms = foldl addToTable (SymbolTable (M.empty) (M.empty) (M.empty) (M.empty) 0 0) ms
