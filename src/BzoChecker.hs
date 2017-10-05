8d384a8ba305c729b5f57cf03f3dc2100176b8b2
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-10-05 20:28:36 +0000

    Started adding a function to translate TypeAST to BzoType

--- src/BzoChecker.hs
+++ src/BzoChecker.hs
@@ -164,4 +164,43 @@ getFIdSet (SymbolTable iids fids itab ftab dmid itop ftop) imps lnks =
 
 
 
---getIdSet :: SymbolTable -> [Int64] -> T.Text -> [Int64]
+-- Hash values are temporary for now
+constructType :: SymbolTable -> TypeAST -> Either [BzoErr] BzoType
+constructType st (TA_Nil    _      ) = Right $ BT_Nil 0
+constructType st (TA_IntLit _ i    ) = Right $ BT_Int 1 i
+constructType st (TA_FltLit _ f    ) = Right $ BT_Flt 2 f
+constructType st (TA_StrLit _ s    ) = Right $ BT_Str 3 (T.pack s)
+constructType st (TA_Arr    _ sz t ) =
+  case (constructType st t) of
+    Left errs -> Left  errs
+    Right typ -> Right (BT_Arr   4 typ (map fromInteger sz))
+
+constructType st (TA_Cmpd   _    ts) =
+  let types = map (constructType st) ts
+  in case (E.lefts types) of
+      []      -> Right $ BT_Cmpd 5 (E.rights types)
+      ers     -> Left  $ concat ers
+
+constructType st (TA_Poly   _    ts) =
+  let types = map (constructType st) ts
+  in case (E.lefts types) of
+      []      -> Right $ BT_Poly 6 (E.rights types)
+      ers     -> Left  $ concat ers
+
+constructType st (TA_Expr   _ hd tl) =
+  let headtype = (constructType st) hd
+      tailtype = (constructType st) tl
+  in case (headtype, tailtype) of
+      (Right h, Right t) -> Right $ BT_Expr 7 h t
+      (Left  h, Right t) -> Left h
+      (Right h, Left  t) -> Left t
+      (Left  h, Left  t) -> Left (h ++ t)
+
+constructType st (TA_FnTy   _ it xt) =
+  let intype = (constructType st) it
+      extype = (constructType st) xt
+  in case (intype, extype) of
+      (Right i, Right x) -> Right $ BT_Expr 7 i x
+      (Left  i, Right x) -> Left i
+      (Right i, Left  x) -> Left x
+      (Left  i, Left  x) -> Left (i ++ x)
