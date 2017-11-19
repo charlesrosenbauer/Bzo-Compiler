3947f90cff691c4da7fc4c84c66759320fd14de7
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-11-19 19:25:13 +0000

    Screw it, copying over lexer from the compiler

--- CodeGenTest/src/IRLexer.hs
+++ CodeGenTest/src/IRLexer.hs
@@ -1,6 +1,7 @@
 module IRLexer where
 import Data.Char
-import Data.List
+import Data.List as L
+import Data.Either as E
 import Control.Monad
 import Control.Applicative
 import Data.Text
@@ -14,11 +15,66 @@ import Data.Text
 
 
 
+sepErrs :: [Either a [b]] -> Either a [[b]]
+sepErrs xs =
+  let (ls, rs) = E.partitionEithers xs
+  in  if((L.length ls) /= 0)
+    then Left  $ ls !! 0
+    else Right $ rs
+
+
+
+
+
+
+
+
+
+
+applyListFns :: [(b -> c)] -> ((b -> c) -> a -> d) -> [[a]] -> [[d]]
+applyListFns fs fn xs = L.map (\(f, x) -> L.map (fn f) x) $ L.zip fs xs
+
+
+
+
+
+
+
+
+
+
+concatMapWithErrs :: [a] -> (a -> E.Either b [c]) -> E.Either b [c]
+concatMapWithErrs xs fn =
+  let (ls, rs) = E.partitionEithers $ L.map fn xs
+  in  if((L.length ls) /= 0)
+    then Left  $ ls !! 0
+    else Right $ L.concat rs
+
+
+
+
+
+
+
+
+
+
 data IRPos = IRPos{
-  ir_line   :: Int,
-  ir_column :: Int,
-  ir_fname  :: Text }
+  irLine   :: Int,
+  irColumn :: Int,
+  irFName  :: Text }
   deriving (Eq, Show)
+
+
+
+
+
+
+
+
+
+
+data IRErr = IRErr IRPos Text
 
 
 
@@ -37,18 +93,8 @@ data IRToken
   | CloseBrace IRPos
   | DefFunc    IRPos
   | DefType    IRPos
+  | NewLine    IRPos
   deriving (Eq, Show)
-
-
-
-
-
-
-
-
-
-
-data IRErr = IRErr IRPos Text deriving Show
 
 
 
@@ -60,12 +106,11 @@ data IRErr = IRErr IRPos Text deriving Show
 
 
 data IRLexerState = IRLexerState{
-  lx_line   :: Int,
-  lx_column :: Int,
-  lx_offset :: Int,
-  lx_fname  :: Text,
-  lx_tokens :: [IRToken] }
-  deriving (Eq, Show)
+  lxLine    :: Int,
+  lxColumn  :: Int,
+  lxOffset  :: Int,
+  lxFName   :: Text }
+  deriving Eq
 
 
 
@@ -76,7 +121,19 @@ data IRLexerState = IRLexerState{
 
 
 
-data IRLexer a = IRLexer { irlex :: String -> IRLexerState -> Either [IRErr] (a, IRLexerState) }
+makeIRPos :: IRLexerState -> IRPos
+makeIRPos (IRLexerState l c _ n) = IRPos l c n
+
+
+
+
+
+
+
+
+
+
+newtype IRLexer a = IRLexer { lex :: String -> IRLexerState -> Either IRErr [(a, IRLexerState, String)] }
 
 
 
@@ -88,4 +145,222 @@ data IRLexer a = IRLexer { irlex :: String -> IRLexerState -> Either [IRErr] (a,
 
 
 makeLexErr :: IRLexerState -> IRErr
-makeLexErr (IRLexerState line column offset fname tokens) = (IRErr (IRPos line column fname) (pack "Lexer Error\n"))
+makeLexErr ls =
+  let p = makeIRPos ls
+      l = irLine p
+      c = irColumn p
+      n = irFName p
+  in (IRErr (IRPos l c n) $ pack "Lexer error\n")
+
+
+
+
+
+
+
+
+
+
+runLexer :: String -> IRLexer a -> String -> Either IRErr a
+runLexer fname lx s =
+  case (IRLexer.lex lx s (IRLexerState 1 1 0 $ pack fname)) of
+    Right [(ret,  _, [] )] -> Right  ret
+    Right [(_  , st, _  )] -> Left $ IRErr (IRPos (lxLine st) (lxColumn st) (lxFName st)) $ pack "Failed to consume entire input."
+    Left  err              -> Left err
+    _                      -> Left $ IRErr (IRPos 0 0 $ pack fname) $ pack "Bug detected in runLexer in BzoLexer.hs"
+
+
+
+
+
+
+
+
+
+
+instance Functor IRLexer where
+  fmap f (IRLexer lf) = IRLexer (\s ls ->
+    case lf s ls of
+      Left  err -> Left  err
+      Right lst -> Right [(f a, b, c) | (a, b, c) <- lst] )
+
+
+
+
+
+
+
+
+
+
+instance Applicative IRLexer where
+  pure a = IRLexer (\s ls -> Right [(a, ls, s)])
+  (IRLexer lf1) <*> (IRLexer lf2) = IRLexer (\s ls ->
+    case (lf1 s ls) of
+      Left  err -> Left err
+      Right lst ->
+        let (p1, p23) = unzip $ L.map (\(a, b, c) -> (a, (c, b))) lst
+            out1 = L.map (\(a, b) -> lf2 a b) p23
+            out2 = sepErrs out1
+        in case out2 of
+          Left  err -> Left err
+          Right val -> Right $ L.concat $ applyListFns p1 (\f (a, b, c) -> (f a, b, c)) val   )
+
+
+
+
+
+
+
+
+
+
+instance Monad IRLexer where
+  return    = pure
+  (>>=) p f = IRLexer (\s ls ->
+    let lf = (\(a, ls', s') -> IRLexer.lex (f a) s' ls')
+        ps = (IRLexer.lex p) s ls
+    in case ps of
+      Left  err -> Left err
+      Right xs  -> concatMapWithErrs xs lf )
+
+
+
+
+
+
+
+
+
+
+instance MonadPlus IRLexer where
+  mzero = IRLexer (\_ ls -> Left $ makeLexErr ls)
+  mplus p q = IRLexer (\s ls ->
+    let ps = (IRLexer.lex p s ls)
+        qs = (IRLexer.lex q s ls)
+    in  case (ps, qs) of
+      (Left err,        _) -> Left err
+      (_       , Left err) -> Left err
+      (Right  x, Right  y) -> Right (x ++ y))
+
+
+
+
+
+
+
+
+
+
+instance Alternative IRLexer where
+  empty = mzero
+  (<|>) p q = IRLexer (\s ls ->
+    case (IRLexer.lex p s ls) of
+      Left  _   -> (IRLexer.lex q) s ls
+      Right []  -> (IRLexer.lex q) s ls
+      x         -> x )
+
+
+
+
+
+
+
+
+
+
+moveOne :: IRLexerState -> Char -> IRLexerState
+moveOne (IRLexerState l c o n) ch =
+  if(ch == '\n')
+    then IRLexerState (l+1) 1 (o+1) n
+    else IRLexerState l (c+1) (o+1) n
+
+
+
+
+
+
+
+
+
+
+satisfy :: (Char -> Bool) -> IRLexer Char
+satisfy f = item >>= \ch ->
+  if f ch
+    then return ch
+    else mzero
+
+
+
+
+
+
+
+
+
+
+getLexerState :: IRLexer IRLexerState
+getLexerState = IRLexer $ \s ls -> Right [(ls, ls, s)]
+
+
+
+
+
+
+
+
+
+
+item :: IRLexer Char
+item = IRLexer $ \s ls ->
+  case s of
+    []     -> Right []
+    (c:cs) -> Right [(c, moveOne ls c, cs)]
+
+
+
+
+
+
+
+
+
+
+lexChar :: Char -> IRLexer Char
+lexChar ch = satisfy (\c -> c == ch)
+
+
+
+
+
+
+
+
+
+
+lexCharFrom :: [Char] -> IRLexer Char
+lexCharFrom cs = satisfy (\c -> elem c cs)
+
+
+
+
+
+
+
+
+
+
+lexExceptChar :: Char -> IRLexer Char
+lexExceptChar ch = satisfy (\c -> c /= ch)
+
+
+
+
+
+
+
+
+
+
+lexExceptCharFrom :: [Char] -> IRLexer Char
+lexExceptCharFrom cs = satisfy (\c -> not $ elem c cs)
