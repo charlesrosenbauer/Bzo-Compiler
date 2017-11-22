7935f5f4a2fa6d8e94a1d0ad3ee14d1e4396a7f1
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-11-22 01:07:50 +0000

    Some slight fixes to parsing code

--- CodeGenTest/src/IRParser.hs
+++ CodeGenTest/src/IRParser.hs
@@ -1,6 +1,7 @@
 module IRParser where
 import IRLexer
 import Data.List
+import Data.Text
 
 
 
@@ -13,15 +14,18 @@ import Data.List
 
 data IRParseItem
   = PI_FnHeader IRPos Text Int Int
-  | PI_Fn       IRPos Text Int Int [IRParseItem]
+  | PI_FnDef    IRPos Text Int Int [IRParseItem]
   | PI_TyHeader IRPos Text Int
-  | PI_TyHeader IRPos Text Int [IRParseItem]
+  | PI_TyDef    IRPos Text Int [IRParseItem]
   | PI_PrHeader IRPos Text Int Int Text
-  | PI_Pr       IRPos Text Int Int Text [IRParseItem]
+  | PI_PrDef    IRPos Text Int Int Text [IRParseItem]
   | PI_ExHeader IRPos Text Int Int
-  | PI_Ex       IRPos Text Int Int [IRParseItem]
+  | PI_ExDef    IRPos Text Int Int [IRParseItem]
   | PI_NL       IRPos
   | PI_Ptr      IRPos Int
+  | PI_Arr      IRPos Int
+  | PI_MultiArr IRPos [Int]
+  | PI_Type     IRPos [Int] Int Text
   | PI_Node     IRPos Int Text [IRParseItem]
   | PI_NS       IRPos [IRParseItem]
   | PI_Nodes    IRPos [IRParseItem]
