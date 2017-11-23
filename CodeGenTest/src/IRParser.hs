2e2c16d2882c33b9c2726a9f21bc2ac213f18202
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-11-23 00:40:04 +0000

    Some initial code for parsing rules

--- CodeGenTest/src/IRParser.hs
+++ CodeGenTest/src/IRParser.hs
@@ -1,6 +1,6 @@
 module IRParser where
 import IRLexer
-import Data.List
+import Data.List as L
 import Data.Text
 
 
@@ -27,7 +27,7 @@ data IRParseItem
   | PI_MultiArr IRPos [Int]
   | PI_Type     IRPos [Int] Int Text
   | PI_Node     IRPos Int Text [IRParseItem]
-  | PI_NS       IRPos [IRParseItem]
+  | PI_NS       IRPos [Int]
   | PI_Nodes    IRPos [IRParseItem]
   | PI_Def      IRPos IRParseItem
   | PI_Defs     IRPos [IRParseItem]
@@ -37,3 +37,42 @@ data IRParseItem
   | PI_Str      IRPos Text
   | PI_Int      IRPos Int
   | PI_Token    IRToken
+
+
+
+
+
+
+
+
+
+
+irParseIter :: [IRParseItem] -> Either IRErr IRParseItem
+irParseIter [] = Left $ IRErr (IRPos 1 1 $ pack "????") $ pack "Nothing to parse?"
+
+irParseIter ((PI_Token (NumToken p0 n0))
+            :(PI_Token (NumToken p1 n1)): ts) = irParseIter ((PI_NS    p0 [n1, n0]) : ts)
+
+irParseIter ((PI_NS     p0 ns)
+            :(PI_Token (NumToken p1 n0)): ts) = irParseIter ((PI_NS    p0 (n0: ns)) : ts)
+
+irParseIter ((PI_Token (NumToken p0 n0)): ts) = irParseIter ((PI_NS    p0  [n0]   ) : ts)
+
+irParseIter ((PI_Token (PtrToken p0))
+            :(PI_Token (PtrToken p1))   : ts) = irParseIter ((PI_Ptr   p0  2      ) : ts)
+
+irParseIter ((PI_Ptr    p0 n )
+            :(PI_Token (PtrToken p1   )): ts) = irParseIter ((PI_Ptr   p0  (n + 1)) : ts)
+
+irParseIter ((PI_Ptr    p0 n)           : ts) = irParseIter ((PI_Ptr   p0  1      ) : ts)
+
+
+
+
+
+
+
+
+
+irParse :: [IRToken] -> Either IRErr IRParseItem
+irParse tks = irParseIter $ L.map PI_Token tks
