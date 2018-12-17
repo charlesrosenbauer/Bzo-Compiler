4bcb0c7fa8415cf9b8f6f39576e902a327ba70a4
charlesrosenbauer <charles.rosenbauer@gmail.com>
2018-12-17 14:43:29 -0600

    More robustness for builtin types and function calls

--- backendTest/Backend.hs
+++ backendTest/Backend.hs
@@ -22,6 +22,7 @@ data Expression
   | FltLit  Int   Double
   | StrLit  Int   String
   | BICall [Int] String [Int]
+  | Set     Int   Int
 
 
 
@@ -74,12 +75,14 @@ printExpr (Sub   q is) = (printVar q) ++ " := " ++ (printInter "-" is) ++ "\n"
 printExpr (Mul   q is) = (printVar q) ++ " := " ++ (printInter "*" is) ++ "\n"
 printExpr (Div   q is) = (printVar q) ++ " := " ++ (printInter "/" is) ++ "\n"
 printExpr (Mod   q is) = (printVar q) ++ " := " ++ (printInter "%" is) ++ "\n"
-printExpr (IntLit q i) = (printVar q) ++ " := " ++ (show i) ++ "\n"
-printExpr (FltLit q f) = (printVar q) ++ " := " ++ (show f) ++ "\n"
+printExpr (IntLit q i) = (printVar q) ++ " := int64(" ++ (show i) ++ ")\n"
+printExpr (FltLit q f) = (printVar q) ++ " := float64(" ++ (show f) ++ ")\n"
 printExpr (StrLit q s) = (printVar q) ++ " := \"" ++ s ++ "\"\n"
-printExpr (FnCall qs fn is) = (printRets qs) ++ (printFunc fn) ++ (printPars is) ++ ")"
+printExpr (FnCall [] fn is) = (printFunc fn) ++ (printPars is) ++ "\n"
+printExpr (FnCall qs fn is) = (printRets qs) ++ (printFunc fn) ++ (printPars is) ++ "\n"
 printExpr (BICall [] fn is) = fn ++ (printPars is) ++ "\n"
 printExpr (BICall qs fn is) = (printRets qs) ++ fn ++ (printPars is) ++ "\n"
+printExpr (Set    q i) = (printVar q) ++ " = " ++ (printVar i) ++ "\n"
 
 
 
@@ -103,8 +106,12 @@ printFunc x = "F" ++ (show x)
 
 
 printType :: Int -> String
-printType 0 = "interface{}"
-printType x = "T" ++ (show x)
+printType (-1) = "int8"
+printType (-2) = "int16"
+printType (-3) = "int32"
+printType (-4) = "int64"
+printType   0  = "interface{}"
+printType   x  = "T" ++ (show x)
 
 
 
@@ -145,13 +152,13 @@ printHeader mname imports =
 printFnHeader :: Int -> [(Int, Int)] -> [(Int, Int)] -> String
 printFnHeader 0 ins exs =
   "func main" ++
-    "(" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b)) ins) ++
-  ") (" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b)) exs) ++ ") {\n"
+    "(" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b) ++ ", ") ins) ++
+  ") (" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b) ++ ", ") exs) ++ ") {\n"
 
 printFnHeader fid ins exs =
   "func F" ++ (show fid) ++
-    "(" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b)) ins) ++
-  ") (" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b)) exs) ++ ") {\n"
+    "(" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b) ++ ", ") ins) ++
+  ") (" ++ (concatMap (\(a,b) -> (printVar a) ++ " " ++ (printType b) ++ ", ") exs) ++ ") {\n"
 
 
 
@@ -165,7 +172,7 @@ printFnHeader fid ins exs =
 -- Later, add contents parameter
 printFn :: Int -> [(Int, Int)] -> [(Int, Int)] -> [Expression] -> String
 printFn fid ins exs exprs =
-  printFnHeader fid ins exs ++ (concatMap printExpr exprs) ++ "}\n"
+  printFnHeader fid ins exs ++ (concatMap printExpr exprs) ++ "return\n}\n"
 
 
 
@@ -174,4 +181,6 @@ printFn fid ins exs exprs =
 
 main :: IO ()
 main =
-  writeFile "output.go" ((printHeader "main" ["fmt"]) ++ (printFn 0 [] [] [(IntLit 1 1), (IntLit 2 2), (IntLit 3 3), (Add 4 [1, 2, 3]), (StrLit 5 "1 + 2 + 3 = "), (BICall [] "fmt.Println" [5, 4])]))
+  writeFile "output.go" ((printHeader "main" ["fmt"]) ++
+    (printFn 0 [] [] [(IntLit 1 1), (IntLit 2 2), (FnCall [3] 1 [1, 2]), (StrLit 4 "1 + 2 ="), (BICall [] "fmt.Println" [4, 3])]) ++
+    (printFn 1 [(1, -4), (2, -4)] [(3, -4)] [(Add 4 [1, 2]), (Set 3 4)]))
