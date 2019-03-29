1973b6f61782f8751ac2f0ebf3bfb5f844b7d3ef
charlesrosenbauer <charles.rosenbauer@gmail.com>
2019-03-28 23:37:17 -0500

    Added a few more cases to type printing

--- src/BzoTypeBackend.hs
+++ src/BzoTypeBackend.hs
@@ -16,7 +16,7 @@ import Debug.Trace
 --TODO: Add case for type classes, maybe?
 -- Not 100% sure we even need to account for type classes in the backend.
 printTDef :: Int64 -> Definition -> String
-printTDef i (TypeDef _ _ _ _ ty) = "\ntype T" ++ (show i) ++ (printType ty) ++ "\n"
+printTDef i (TypeDef _ _ _ _ ty) = "\ntype T" ++ (show i) ++ " " ++ (printType ty) ++ "\n"
 printTDef _ _ = ""
 
 
@@ -42,12 +42,16 @@ printType (CmpdType _ ts) =
 
 printType (ArryType _  0 t) = "[]" ++ (printType t)
 printType (ArryType _ sz t) = "[" ++ (show sz) ++ "]" ++ (printType t)
+printType (FuncType _ i  o) = "func (" ++ (show i) ++ ")(" ++ (show o) ++ ")"
+printType (LtrlType _    t) = "T" ++ (show t) ++ " "
+printType (IntType  _    i) = "int64 "
+printType (FltType  _    f) = "float64 "
+printType (StrType  _    s) = "string "
 printType _ = " //<???>"
 
 {-
   TODO:
-  * Add Function types
-  * Handle Literal cases
+  * Handle builtin types
   * Figure out what exactly to do with tvars (probably nothing)
 -}
 
