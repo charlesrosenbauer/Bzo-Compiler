ad7e8f02c9f14b645fb64362c4f925e5ec43f943
charlesrosenbauer <charles.rosenbauer@gmail.com>
2019-03-29 16:22:31 -0500

    Added builtin and void cases for type printing

--- src/BzoTypeBackend.hs
+++ src/BzoTypeBackend.hs
@@ -13,10 +13,47 @@ import Debug.Trace
 
 
 
+
+printTLit :: Int64 -> String
+printTLit  1 = "int8 "
+printTLit  2 = "int16 "
+printTLit  3 = "int32 "
+printTLit  4 = "int64 "
+printTLit  5 = "uint8 "
+printTLit  6 = "uint16 "
+printTLit  7 = "uint32 "
+printTLit  8 = "uint64 "
+printTLit  9 = "float32 "  -- F16
+printTLit 10 = "float32 "
+printTLit 11 = "float64 "
+printTLit 12 = "float32 "  -- P8
+printTLit 13 = "float32 "  -- P16
+printTLit 14 = "float32 "  -- P32
+printTLit 15 = "float64 "  -- P64
+printTLit 16 = "string "
+printTLit 20 = "bool "
+printTLit 21 = "uint8 "
+printTLit 22 = "uint16 "
+printTLit 23 = "uint32 "
+printTLit 24 = "uint64 "
+printTLit 25 = "[2]uint64 "
+printTLit 26 = "[4]uint64 "
+printTLit 27 = "[8]uint64 "
+printTLit  x = "T" ++ (show x) ++ " "
+
+
+
+
+
+
+
+
+
+
 --TODO: Add case for type classes, maybe?
 -- Not 100% sure we even need to account for type classes in the backend.
 printTDef :: Int64 -> Definition -> String
-printTDef i (TypeDef _ _ _ _ ty) = "\ntype T" ++ (show i) ++ " " ++ (printType ty) ++ "\n"
+printTDef i (TypeDef tid _ _ _ ty) = "\n//" ++ (show tid) ++ "\ntype T" ++ (show i) ++ " " ++ (printType ty) ++ "\n"
 printTDef _ _ = ""
 
 
@@ -43,10 +80,12 @@ printType (CmpdType _ ts) =
 printType (ArryType _  0 t) = "[]" ++ (printType t)
 printType (ArryType _ sz t) = "[" ++ (show sz) ++ "]" ++ (printType t)
 printType (FuncType _ i  o) = "func (" ++ (show i) ++ ")(" ++ (show o) ++ ")"
-printType (LtrlType _    t) = "T" ++ (show t) ++ " "
+printType (LtrlType _    t) = printTLit t
+printType (BITyType _    t) = printTLit t
 printType (IntType  _    i) = "int64 "
 printType (FltType  _    f) = "float64 "
 printType (StrType  _    s) = "string "
+printType (VoidType _     ) = "bool "  -- Until I find a better type to use.
 printType _ = " //<???>"
 
 {-
