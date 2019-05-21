d9409f6cd740b3502ff4b77fbf0bad30bfc6f20b
charlesrosenbauer <charles.rosenbauer@gmail.com>
2019-05-20 23:45:42 -0500

    Added poly checking to typechecker

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -26,12 +26,28 @@ import Debug.Trace
 getTyId :: DefinitionTable -> Int64 -> Text
 getTyId (DefinitionTable defs _ _ _) t = identifier $ defs M.! t
 
+{-
+  TODO:
+    * Subtyping checks
+    * Typeclass checks
+    * Value checks for ints, flts, and strings
+    * Maketype checks
+    * TVar types
+    * Function literal checks
+    * Array type checks
+    * Tuple-array casting
+    * Builtin checks
+-}
 checkType :: DefinitionTable -> (TypeHeader, Type) -> (TypeHeader, Type) -> [BzoErr]
 checkType _ (_, VoidType    _) (_, VoidType    _) = []
 checkType _ (_, IntType  p i0) (_, IntType  _ i1) = ife (i0 == i1) [] [TypeErr p $ pack $ "Integer types " ++ (show i0) ++ " and " ++ (show i1) ++ " do not match."]
 checkType _ (_, FltType  p f0) (_, FltType  _ f1) = ife (f0 == f1) [] [TypeErr p $ pack $ "Integer types " ++ (show f0) ++ " and " ++ (show f1) ++ " do not match."]
 checkType _ (_, StrType  p s0) (_, StrType  _ s1) = ife (s0 == s1) [] [TypeErr p $ pack $ "Integer types " ++ (show s0) ++ " and " ++ (show s1) ++ " do not match."]
-checkType d (_, LtrlType p t0) (_, LtrlType _ t1) = ife (t0 == t1) [] [TypeErr p $ pack $ "Types " ++ (show $ getTyId d t0) ++ " and " ++ (show $ getTyId d t1) ++ " do not match."]
+checkType d (_, LtrlType p t0) (_, LtrlType _ t1) =
+  let
+      -- TODO: Add extra cases for typeclass checking, subtype checking, etc.
+  in ife (t0 == t1) [] [TypeErr p $ pack $ "Types " ++ (show $ getTyId d t0) ++ " and " ++ (show $ getTyId d t1) ++ " do not match."]
+
 checkType d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
   let
       xlen :: Int
@@ -50,5 +66,16 @@ checkType d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
       errs = ife (L.null samelength) each samelength
 
   in errs
+
+-- These two will probably spit out some gnarly error messages. Tech debt?
+checkType d (h0, PolyType _ xs) (h1, PolyType p ys) = L.concatMap (\x -> checkType d (h0, x) (h1, PolyType p ys)) xs
+checkType d (h0, t) (h1, PolyType _ ys) =
+  let
+      each :: [[BzoErr]]
+      each = L.map (\y -> checkType d (h0, t) (h1, y)) ys
+
+  in if (L.any L.null each)
+      then []
+      else L.concat each
 
 checkType _ (_, x) (_, _) = [TypeErr (typos x) $ pack "Type Mismatch"]
