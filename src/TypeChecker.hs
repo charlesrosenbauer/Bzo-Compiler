9dbac840c1b4509ac9bc49354151332afd7c0429
charlesrosenbauer <charles.rosenbauer@gmail.com>
2019-05-25 02:17:21 -0500

    Added a way to keep track of if types are used in inputs or outputs; outputs have weaker constraints.

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -38,30 +38,32 @@ getTyId (DefinitionTable defs _ _ _) t = identifier $ defs M.! t
     * Tuple-array casting
     * Builtin checks
 -}
-checkType' :: DefinitionTable -> (TypeHeader, Type) -> (TypeHeader, Type) -> ([BzoErr], [(TVId, Type)])
-checkType' _ (_, VoidType    _) (_, VoidType    _) = ([], [])
-checkType' _ (_, IntType  p i0) (_, IntType  _ i1) = (ife (i0 == i1) [] [TypeErr p $ pack $ "Integer types " ++ (show i0) ++ " and " ++ (show i1) ++ " do not match."], [])
-checkType' _ (_, FltType  p f0) (_, FltType  _ f1) = (ife (f0 == f1) [] [TypeErr p $ pack $ "Integer types " ++ (show f0) ++ " and " ++ (show f1) ++ " do not match."], [])
-checkType' _ (_, StrType  p s0) (_, StrType  _ s1) = (ife (s0 == s1) [] [TypeErr p $ pack $ "Integer types " ++ (show s0) ++ " and " ++ (show s1) ++ " do not match."], [])
+data IOKind = InKind | ExKind
 
-checkType' d (h0,FuncType _ i0 o0) (h1,FuncType _ i1 o1) =
+checkType' :: IOKind -> DefinitionTable -> (TypeHeader, Type) -> (TypeHeader, Type) -> ([BzoErr], [(TVId, Type, IOKind)])
+checkType' _ _ (_, VoidType    _) (_, VoidType    _) = ([], [])
+checkType' _ _ (_, IntType  p i0) (_, IntType  _ i1) = (ife (i0 == i1) [] [TypeErr p $ pack $ "Integer types " ++ (show i0) ++ " and " ++ (show i1) ++ " do not match."], [])
+checkType' _ _ (_, FltType  p f0) (_, FltType  _ f1) = (ife (f0 == f1) [] [TypeErr p $ pack $ "Integer types " ++ (show f0) ++ " and " ++ (show f1) ++ " do not match."], [])
+checkType' _ _ (_, StrType  p s0) (_, StrType  _ s1) = (ife (s0 == s1) [] [TypeErr p $ pack $ "Integer types " ++ (show s0) ++ " and " ++ (show s1) ++ " do not match."], [])
+
+checkType' _ d (h0,FuncType _ i0 o0) (h1,FuncType _ i1 o1) =
   let
       inErrs :: [BzoErr]
-      inVars :: [(TVId, Type)]
-      (inErrs, inVars) = (checkType' d (h0, i0) (h1, i1))
+      inVars :: [(TVId, Type, IOKind)]
+      (inErrs, inVars) = (checkType' InKind d (h0, i0) (h1, i1))
 
       exErrs :: [BzoErr]
-      exVars :: [(TVId, Type)]
-      (exErrs, exVars) = (checkType' d (h0, o0) (h1, o1))
+      exVars :: [(TVId, Type, IOKind)]
+      (exErrs, exVars) = (checkType' ExKind d (h0, o0) (h1, o1))
 
   in ((inErrs ++ exErrs), (inVars ++ exVars))
 
-checkType' d (_, LtrlType p t0) (_, LtrlType _ t1) =
+checkType' _ d (_, LtrlType p t0) (_, LtrlType _ t1) =
   let
       -- TODO: Add extra cases for typeclass checking, subtype checking, etc.
   in (ife (t0 == t1) [] [TypeErr p $ pack $ "Types " ++ (show $ getTyId d t0) ++ " and " ++ (show $ getTyId d t1) ++ " do not match."], [])
 
-checkType' d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
+checkType' k d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
   let
       xlen :: Int
       xlen = L.length xs
@@ -73,8 +75,8 @@ checkType' d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
       samelength = ife (xlen == ylen) [] [TypeErr p $ pack $ "Expeted tuple with " ++ (show xlen) ++ " elements, found " ++ (show ylen) ++ "elements instead."]
 
       eacherr :: [BzoErr]
-      eachvar :: [(TVId, Type)]
-      (eacherr, eachvar) = concatUnzip $ L.map (\(a,b) -> checkType' d (h0,a) (h1,b)) $ L.zip xs ys
+      eachvar :: [(TVId, Type, IOKind)]
+      (eacherr, eachvar) = concatUnzip $ L.map (\(a,b) -> checkType' k d (h0,a) (h1,b)) $ L.zip xs ys
 
       errs :: [BzoErr]
       errs = ife (L.null samelength) eacherr samelength
@@ -82,10 +84,10 @@ checkType' d (h0,CmpdType p xs) (h1,CmpdType _ ys) =
   in (errs, eachvar)
 
 -- These two will probably spit out some gnarly error messages. Tech debt?
-checkType' d (h0, PolyType _ xs) (h1, PolyType p ys) = (L.concatMap (\x -> checkType d (h0, x) (h1, PolyType p ys)) xs, [])
-checkType' d (h0, t) (h1, PolyType _ ys) =
+checkType' k d (h0, PolyType _ xs) (h1, PolyType p ys) = (L.concatMap (\x -> checkType d (h0, x) (h1, PolyType p ys)) xs, [])   -- TODO: don't forget this case
+checkType' k d (h0, t) (h1, PolyType _ ys) =
   let
-      -- Not quite sure how to tvar collection here yet. Seems a bit complex.
+      -- Vars should be filtered to only those with no corresponding errors.
       each :: [[BzoErr]]
       each = L.map (\y -> checkType d (h0, t) (h1, y)) ys
 
@@ -93,10 +95,12 @@ checkType' d (h0, t) (h1, PolyType _ ys) =
       then ([], [])
       else (L.concat each, [])
 
-checkType' _ (_, x) (_, _) = ([TypeErr (typos x) $ pack "Type Mismatch"], [])
+checkType' k d (h0, TVarType p v) (h1, t) = ([], [(v, t, k)])
+
+checkType' _ _ (_, x) (_, _) = ([TypeErr (typos x) $ pack "Type Mismatch"], [])
 
 
 
 
 checkType :: DefinitionTable -> (TypeHeader, Type) -> (TypeHeader, Type) -> [BzoErr]
-checkType d a b = fst $ checkType' d a b
+checkType d a b = fst $ checkType' InKind d a b
